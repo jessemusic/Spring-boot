7699b34b7a1dbd23c922053d569cce92512f08d5
jessemusic <jmc_cursos@hotmail.com>
2021-05-20 11:48:15 -0300

    Projeto iniciado

---
+++ mudijesse/src/main/java/br/com/jmccursos/tecnologia/mudijesse/MudijesseApplication.java
@@ -0,0 +1,13 @@
+package br.com.jmccursos.tecnologia.mudijesse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudijesseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudijesseApplication.class, args);
+	}
+
+}
---
+++ mudijesse/src/test/java/br/com/jmccursos/tecnologia/mudijesse/MudijesseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jmccursos.tecnologia.mudijesse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudijesseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
