deb6014894ed3522270c7cadbf0acf34730ee4fb
jessemusic <jmc_cursos@hotmail.com>
2021-05-27 13:45:25 -0300

    DelegatingPasswordEncoder login e logout

---
+++ mudijesse/src/main/java/br/com/jmccursos/tecnologia/mudijesse/controller/LoginController.java
@@ -0,0 +1,17 @@
+package br.com.jmccursos.tecnologia.mudijesse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+
+}
