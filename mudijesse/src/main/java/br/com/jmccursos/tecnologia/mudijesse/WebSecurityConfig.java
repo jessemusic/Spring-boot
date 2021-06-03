3b9f1d58686037a20bc4a1fbac06b1f3c87c5879
jessemusic <jmc_cursos@hotmail.com>
2021-06-03 00:28:21 -0300

    Erros na tela

--- mudijesse/src/main/java/br/com/jmccursos/tecnologia/mudijesse/WebSecurityConfig.java
+++ mudijesse/src/main/java/br/com/jmccursos/tecnologia/mudijesse/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.csrf().disable()
+		
 		.authorizeRequests()		
 		.antMatchers("/home/**")
 				.permitAll()
@@ -35,7 +35,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
 		.logout(logout ->{
 			logout.logoutUrl("/logout")
 				.logoutSuccessUrl("/home");
-		});
+		}).csrf().disable();
 	}
 	
 	@Override
